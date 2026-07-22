ad17f73286b5cdc8cd10e5a45abcdc98106b35c9
Faros <114036707+a-faros@users.noreply.github.com>
2026-07-23 07:30:10 +0800

    Automated bulk repository update

--- src/BrandingServiceProvider.php
+++ src/BrandingServiceProvider.php
@@ -4,31 +4,31 @@ declare(strict_types=1);
 
 namespace Rimba\Branding;
 
-use Rimba\Base\BitesServiceProvider;
 use Filament\Support\Facades\FilamentView;
 use Filament\View\PanelsRenderHook;
-
+use Rimba\Base\BitesServiceProvider;
 
 class BrandingServiceProvider extends BitesServiceProvider
 {
-    protected string $configFile = __DIR__ . '/../config/bites.php';
-    protected string $viewsPath = __DIR__ . '/../resources/views';
+    protected string $configFile = __DIR__.'/../config/bites.php';
+
+    protected string $viewsPath = __DIR__.'/../resources/views';
 
     protected function bootPackage(): void
     {
         FilamentView::registerRenderHook(
             PanelsRenderHook::AUTH_LOGIN_FORM_AFTER,
-            fn() => view('panel.branding')
+            fn () => view('panel.branding')
         );
         FilamentView::registerRenderHook(
             PanelsRenderHook::AUTH_REGISTER_FORM_AFTER,
-            fn() => view('panel.branding')
+            fn () => view('panel.branding')
         );
 
     }
+
     protected function registerPackage(): void
     {
         //
     }
-
 }
