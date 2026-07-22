0b65b75916bd8bf082244c942081ff492375a7a2
Faros <114036707+a-faros@users.noreply.github.com>
2026-07-22 15:32:38 +0800

    Automated bulk repository update

---
+++ resources/views/panel/branding.blade.php
@@ -0,0 +1,61 @@
+<div id="slogan">
+    <h2>{{ config('bites.slogan') }}</h2>
+</div>
+
+<style>
+    body {
+        background: url('{{ asset('images/atm.jpg') }}') no-repeat center center fixed;
+        background-size: cover;
+    }
+
+    main {
+        position: relative;
+    }
+
+    #helpers-translate {
+        position: absolute;
+        right: 45px;
+        top: 20px;
+    }
+
+    #helpers-guide {
+        position: absolute;
+        right: 20px;
+        top: 20px;
+    }
+
+    @media screen and (min-height: 878px) and (min-width: 1024px) {
+        main {
+            position: absolute;
+            left: 100px;
+            top: 50px;
+        }
+
+        #slogan {
+            position: fixed;
+            right: 50px;
+            bottom: 25px;
+            color: var(--primary-600); 
+            font-family: Arial, sans-serif;
+            font-size: 3em;
+            font-weight: bold;
+            z-index: 10;
+            width: 700px;
+            white-space: normal;
+            text-align: center;
+        }
+
+    }
+</style>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        // Redirects the logo/header to the dashboard when clicked
+        const headers = document.querySelectorAll('.fi-simple-header');
+        headers.forEach(header => {
+            header.addEventListener('click', function() {
+                window.location.href = "{{ route('filament.staff.pages.dashboard') }}";
+            });
+        });
+    });
+</script>
---
+++ src/BrandingServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rimba\Branding;
+
+use Rimba\Base\BitesServiceProvider;
+use Filament\Support\Facades\FilamentView;
+use Filament\View\PanelsRenderHook;
+
+
+class BrandingServiceProvider extends BitesServiceProvider
+{
+    protected string $configFile = __DIR__ . '/../config/bites.php';
+    protected string $viewsPath = __DIR__ . '/../resources/views';
+
+    protected function bootPackage(): void
+    {
+        FilamentView::registerRenderHook(
+            PanelsRenderHook::AUTH_LOGIN_FORM_AFTER,
+            fn() => view('panel.branding')
+        );
+        FilamentView::registerRenderHook(
+            PanelsRenderHook::AUTH_REGISTER_FORM_AFTER,
+            fn() => view('panel.branding')
+        );
+
+    }
+    protected function registerPackage(): void
+    {
+        //
+    }
+
+}
