2c45e4a1c75a2192d367c437898acc494835d828
Faros <114036707+a-faros@users.noreply.github.com>
2026-07-27 10:12:36 +0800

    Update 2026-07-27 10:12

--- src/BrandingServiceProvider.php
+++ src/BrandingServiceProvider.php
@@ -4,31 +4,29 @@ declare(strict_types=1);
 
 namespace Rimba\Branding;
 
+use Rimba\Base\Services\BitesServiceProvider;
 use Filament\Support\Facades\FilamentView;
 use Filament\View\PanelsRenderHook;
-use Rimba\Base\Services\BitesServiceProvider;
+
 
 class BrandingServiceProvider extends BitesServiceProvider
 {
-    protected string $configFile = __DIR__.'/../config/bites.php';
-
-    protected string $viewsPath = __DIR__.'/../resources/views';
 
     protected function bootPackage(): void
     {
         FilamentView::registerRenderHook(
             PanelsRenderHook::AUTH_LOGIN_FORM_AFTER,
-            fn () => view('panel.branding')
+            fn() => view('panel.branding')
         );
         FilamentView::registerRenderHook(
             PanelsRenderHook::AUTH_REGISTER_FORM_AFTER,
-            fn () => view('panel.branding')
+            fn() => view('panel.branding')
         );
 
     }
-
     protected function registerPackage(): void
     {
         //
     }
+
 }
